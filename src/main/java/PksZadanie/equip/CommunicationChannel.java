3f1aec935a8735b0e8df70c8a05d2c8da02e6b5e
Mathis <Mathis@Mathis_pc>
2014-03-26 21:17:49 +0100

    Final version (hopefully) 1.2

--- src/main/java/PksZadanie/equip/CommunicationChannel.java
+++ src/main/java/PksZadanie/equip/CommunicationChannel.java
@@ -20,6 +20,20 @@ public class CommunicationChannel {
     Frame frame;
     private Integer completed = 0;
     private Integer commId;
+    Integer rst = 0;
+    Integer srcPsyn = 0;
+    Integer srcPfin3 = 0;
+    Integer srcPfin4 = 0;
+    Integer open = 0;
+    Integer synOK = 0;
+    Integer finOK3 = 0;
+    Integer finOK4 = 0;
+    Integer finOKK4 = 0;
+    Integer close3 = 0;
+    Integer close4 = 0;
+    Integer foundSyn = 0;
+    Integer foundFin3 = 0;
+    Integer foundFin4 = 0;
 
     public CommunicationChannel(Frame frame) {
         this.frame = frame;
@@ -35,20 +49,7 @@ public class CommunicationChannel {
     }
 
     public void checkCompleted() {
-        Integer rst = 0;
-        Integer srcPsyn = 0;
-        Integer srcPfin3 = 0;
-        Integer srcPfin4 = 0;
-        Integer open = 0;
-        Integer synOK = 0;
-        Integer finOK3 = 0;
-        Integer finOK4 = 0;
-        Integer finOKK4 = 0;
-        Integer close3 = 0;
-        Integer close4 = 0;
-        Integer foundSyn = 0;
-        Integer foundFin3 = 0;
-        Integer foundFin4 = 0;
+
         for (Frame temp : tcpCommList) {
             if (DataTypeHelper.getTcpPortFlags(temp).contains("SYN") && foundSyn.equals(0)) {
                 foundSyn = 1;
@@ -66,52 +67,21 @@ public class CommunicationChannel {
                 open = 1;
                 //System.out.println("nasiel som ACK");
 
-            } else if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && foundFin3.equals(0)) {
-                foundFin3 = 1;
-                if (srcPfin3 == 0) {
-                    srcPfin3 = temp.getIpv4parser().getTcpParser().getSourcePort();
-                }
-
-            } else if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && foundFin3.equals(1)) {
-                if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && srcPfin3.equals(temp.getIpv4parser().getTcpParser().getDestinationPort())) {
-                    finOK3 = 1;
-                }
-            } else if (DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && srcPfin3.equals(temp.getIpv4parser().getTcpParser().getSourcePort()) && finOK3.equals(1) && !close3.equals(1)) {
-                close3 = 1;
-     
-
-            } else if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && foundFin4.equals(0)) {
-                foundFin4 = 1;
-                if (srcPfin4 == 0) {
-                    srcPfin4 = temp.getIpv4parser().getTcpParser().getSourcePort();
-                }
-                // System.out.println("nasiel som FIN a ACK");
-            } else if (DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && foundFin4.equals(1) && !finOK4.equals(1) && !finOKK4.equals(1)) {
-                if (DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && srcPfin4.equals(temp.getIpv4parser().getTcpParser().getDestinationPort())) {
-                    finOK4 = 1;
-                }
-                //System.out.println("nasiel som ACK");
-            } else if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && srcPfin4.equals(temp.getIpv4parser().getTcpParser().getDestinationPort()) && finOK4.equals(1)) {
-                finOKK4 = 1;
-                // System.out.println("nasiel som FIN a ACK opacne");
-            } else if (DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && !close4.equals(1) && finOK4.equals(1) && finOKK4.equals(1) && srcPfin4.equals(temp.getIpv4parser().getTcpParser().getSourcePort())) {
-                close4 = 1;
-                //   System.out.println("nasiel som ACK naspet");
-                
-
             } else if (DataTypeHelper.getTcpPortFlags(temp).contains("RST")) {
                 rst++;
-         
-
             }
+            segment3(temp);
+            segment4(temp);
+            
+            
         }
-        if ((open.equals(1) && (close3.equals(1) || close4.equals(1))) || rst > 0) {
+        if ((open.equals(1) && (close3.equals(1) || close4.equals(1))) || (open.equals(1) && rst > 0)) {
             completed = 1;
             if (rst > 0) {
-          closingType = "reset close";
+                closingType = "reset close";
             }
             if (close3.equals(1)) {
-               closingType = "three segment close";
+                closingType = "three segment close";
             }
             if (close4.equals(1)) {
                 closingType = "four segment connection close";
@@ -121,46 +91,48 @@ public class CommunicationChannel {
         if (closingType == null) {
             closingType = "incomplete connection";
         }
-        /*
-         for (Frame temp : tcpCommList) {
-         if (DataTypeHelper.getTcpPortFlags(temp).contains("SYN")) {
-         foundSyn = 1
-         if (srcPsyn == 0) {
-         srcPsyn = temp.getIpv4parser().getTcpParser().getSourcePort();
-         } else {
-         if (DataTypeHelper.getTcpPortFlags(temp).contains("SYN") && srcPsyn.equals(temp.getIpv4parser().getTcpParser().getDestinationPort())) {
-         open = 1;
-         }
-         }
-         } else if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN")) {
-         if (srcPfin == 0) {
-         srcPfin = temp.getIpv4parser().getTcpParser().getSourcePort();
-         } else {
-         if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && srcPfin.equals(temp.getIpv4parser().getTcpParser().getDestinationPort())) {
-         close = 1;
-         }
-         }
-         } else if (DataTypeHelper.getTcpPortFlags(temp).contains("RST")) {
-         rst++;
-         }
-         }
-         if((open.equals(1) && close.equals(1)) || rst > 0){
-         completed = 1;
-         }
-            
-         */
-        /*
-         for (Frame temp : tcpCommList) {
-         if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN")) {
-         fin++;
-         } else if (DataTypeHelper.getTcpPortFlags(temp).contains("RST")) {
-         rst++;
-         }
-         }
-         if (fin > 1 || rst > 0) {
-         completed = 1;
-         }
-         */
+
+    }
+
+    public void segment4(Frame temp) {
+        if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && foundFin4.equals(0)) {
+            foundFin4 = 1;
+            if (srcPfin4 == 0) {
+                srcPfin4 = temp.getIpv4parser().getTcpParser().getSourcePort();
+            }
+            // System.out.println("nasiel som FIN a ACK");
+        } else if (!DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && foundFin4.equals(1) && !finOK4.equals(1) && !finOKK4.equals(1)) {
+            if (DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && srcPfin4.equals(temp.getIpv4parser().getTcpParser().getDestinationPort())) {
+                finOK4 = 1;
+            }
+            //System.out.println("nasiel som ACK");
+        } else if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && srcPfin4.equals(temp.getIpv4parser().getTcpParser().getDestinationPort()) && finOK4.equals(1)) {
+            finOKK4 = 1;
+            // System.out.println("nasiel som FIN a ACK opacne");
+        } else if (DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && !close4.equals(1) && finOK4.equals(1) && finOKK4.equals(1) && srcPfin4.equals(temp.getIpv4parser().getTcpParser().getSourcePort())) {
+            close4 = 1;
+            //   System.out.println("nasiel som ACK naspet");
+
+        }
+    }
+
+    public void segment3(Frame temp) {
+        if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && foundFin3.equals(0)) {
+            foundFin3 = 1;
+            if (srcPfin3 == 0) {
+                srcPfin3 = temp.getIpv4parser().getTcpParser().getSourcePort();
+                System.out.println("nasiel som FIN ACK");
+            }
+
+        } else if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && foundFin3.equals(1)) {
+            if (DataTypeHelper.getTcpPortFlags(temp).contains("FIN") && srcPfin3.equals(temp.getIpv4parser().getTcpParser().getDestinationPort())) {
+                finOK3 = 1;
+                System.out.println("nasiel som FIN ACK opacne");
+            }
+        } else if (DataTypeHelper.getTcpPortFlags(temp).contains("ACK") && srcPfin3.equals(temp.getIpv4parser().getTcpParser().getSourcePort()) && finOK3.equals(1) && !close3.equals(1)) {
+            close3 = 1;
+            System.out.println("nasiel som ACK");
+        }
     }
 
     public Integer getCommId() {
